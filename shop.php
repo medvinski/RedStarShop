0cf7cd8b4f928d2600185f780f23b3570553df34
medvinski <103368186+medvinski@users.noreply.github.com>
2022-12-03 00:12:31 +0100

    update 03.12.22

--- shop.php
+++ shop.php
@@ -12,6 +12,7 @@ function function_alert($message) {
 function_alert("Kim Jong Un asks people to eat less till 2025 - Please limit your food purchases!");
 
 include("header.html");
+@include "config.php";
 
         
 ?> 
@@ -23,11 +24,48 @@ include("header.html");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="stylesheets/shop.css">
+    <link rel="stylesheet" href="stylesheets/admin.css">
+    
     
     <title>Homepage</title>
 </head>
 <body>
+<?php
+$select = mysqli_query($conn, "SELECT * FROM products");
+?>
+<div class="product-display">
+    <table class ="product-display-table">
+     <thead>
+        
+        <tr>
+            <td>Image</td>
+            <td>Name</td>
+            <td>Price</td>
+            <td>Action</td>
 
+
+        </tr>
+      </thead>
+      <?php
+
+      while($row =mysqli_fetch_assoc($select)){
+
+      ?>
+    <tr>
+       <td><img src="product_images/<?php echo $row['image']; ?>" height="100" alt=""></td>
+       <td><?php echo $row['name'];?></td>
+       <td><?php echo $row['price'];?></td>
+       <td>
+        <a href="admin_edit.php?edit=<?php echo $row['id'];?>" class="btn">Edit</a>
+        <a href="admin.php?delete=<?php echo $row['id'];?>" class="btn">Delete</a>
+        
+        
+       </td>
+
+
+    </tr>
+
+    <?php };?>
 
 
 
