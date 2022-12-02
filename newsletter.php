db3a16693dbcb5bcd4d473327fe3f04ed0aa93d2
medvinski <103368186+medvinski@users.noreply.github.com>
2022-12-02 22:36:21 +0100

    Master

---
+++ newsletter.php
@@ -0,0 +1,35 @@
+<?php
+
+
+function function_alert($message) {
+      
+    // Display the alert box 
+    echo "<script>alert('$message');</script>";
+}
+  
+  
+// Function call
+function_alert("Kim Jong Un asks people to eat less till 2025 - Please limit your food purchases!");
+
+include("header.html");
+
+        
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheets/shop.css">
+    
+    <title>DPR Newsletter</title>
+</head>
+<body>
+
+<iframe src="files/november-2022-newsletter.pdf" width="100%" height="100%">
+
+
+</body>
+</html>
