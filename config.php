ee029d85567590e2b3e7fe4e7356055236b0ce1f
medvinski <103368186+medvinski@users.noreply.github.com>
2022-12-04 22:47:43 +0100

    22:47

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 
 <?php
-$conn = mysqli_connect("localhost", "root","","shop") or die('connection failed');
+$conn = mysqli_connect("u-cdbr-west-03.cleardb.net", "b6416af60d0060","451201f3","heroku_f8df3f8643957a5") or die('connection failed');
 ?> 
